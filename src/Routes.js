9035aefceebd5fb969e7e443f150dc0fd416c3ba
JoseLAmador <josel.amador.s@gmail.com>
2017-11-01 12:58:38 -0600

    ready

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Root} from 'native-base';
+import {Router, Scene} from 'react-native-router-flux';
+import Login from "./components/login/Login";
+import Log from "./components/login/Log";
+import Recover from "./components/login/Recover";
+
+
+const Routes = () => {
+    return (
+        <Root>
+          <Router>
+            <Scene key="root">
+              <Scene key="Log" header={null} component={Log} />
+              <Scene key="Login" header={null} component={Login} />
+              <Scene key="Recover" header={null} component={Recover} />
+
+            </Scene>
+          </Router>
+        </Root>
+    );
+}
+export default Routes;
---
+++ src/components/principal/Principal.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class Principal extends Component{
+    render(){
+        return(
+            <Text>dasd</Text>
+        )
+    }
+}
